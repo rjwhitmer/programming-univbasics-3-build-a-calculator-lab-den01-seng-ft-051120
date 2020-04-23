4f8b085520b36589cdf1608627c27f474a717484
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-23 21:00:56 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  solution = num1 + num2
+  solution
 end
 
 def subtraction(num1, num2)
-
+  solution = num1 - num2
+  solution
 end
 
 def division(num1, num2)
-
+  solution = (num1)/(num2)
+  solution
 end
 
 def multiplication(num1, num2)
-
+  solution = num1 * num2
+  solution
 end
 
 def modulo(num1, num2)
-
+  solution = num1 % num2
+  solution
 end
 
 def square_root(num)
-
+  solution = Math.sqrt(num)
+  solution
 end
