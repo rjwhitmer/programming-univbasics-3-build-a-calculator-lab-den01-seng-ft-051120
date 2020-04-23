477feab54eb25c6f23e3a481ee4d6353c57a0b65
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-23 20:56:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,11 +4,13 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  solution = num1 - num2
+  solution
 end
 
 def division(num1, num2)
-
+  solution = num2 / num1
+  solution
 end
 
 def multiplication(num1, num2)
